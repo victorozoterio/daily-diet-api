c9597e9bd185df00b5f9fccf8ab31953b9ba76b9
Victor Ozoterio <victorozoterio@gmail.com>
2024-09-03 15:24:14 -0300

    feat: criando tabela de usuários

---
+++ src/migrations/20240903181113_create-users.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('users', (table) => {
+        table.uuid('uuid').primary()
+        table.text('name').notNullable()
+        table.text('email').notNullable()
+        table.text('password').notNullable()
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+        table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('users')
+}
+
