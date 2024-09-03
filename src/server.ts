57f0ac250be665b48375f5362818b2532146caee
Victor Ozoterio <victorozoterio@gmail.com>
2024-09-03 14:07:01 -0300

    feat: configurando o Biome e o Husky

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import fastify from 'fastify';
 
-const app = fastify()
+const app = fastify();
 
 app.get('/', () => {
-    return "Hello World"
-})
+  return 'Hello World';
+});
 
-app.listen({port: 3000}).then(() => {
-    console.log('Server is running!')
-})
+app.listen({ port: 3000 }).then(() => {
+  console.log('Server is running!');
+});
