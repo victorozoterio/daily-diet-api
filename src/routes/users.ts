8380609b873088a1c5d82fded6773e5a75f223df
Victor Ozoterio <victorozoterio@gmail.com>
2024-09-09 11:26:19 -0300

    feat: rota de criação de usuários

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,27 @@
 import { FastifyInstance } from 'fastify';
 import { knex } from '../config';
+import { z } from 'zod';
+import { randomUUID } from 'node:crypto';
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const tables = await knex('sqlite_schema').select('*');
-    return tables;
+  app.post('/', async (request, reply) => {
+    const createUsersBodySchema = z.object({
+      name: z.string(),
+      email: z.string(),
+      password: z.string(),
+    });
+
+    const { name, email, password } = createUsersBodySchema.parse(request.body);
+
+    const [createdUser] = await knex('users')
+      .insert({
+        uuid: randomUUID(),
+        name,
+        email,
+        password,
+      })
+      .returning('*');
+
+    return reply.status(201).send(createdUser);
   });
 }
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { usersRoutes } from './routes/users';
 
 const app = fastify();
 
-app.register(usersRoutes);
+app.register(usersRoutes, { prefix: 'users' });
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server is running on port ${env.PORT}`);
