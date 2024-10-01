e0b26300a93490dc53cec2a55b486a46d99d5234
Victor Ozoterio <victorozoterio@gmail.com>
2024-10-01 18:19:10 -0300

    feat: tratando erros ao criar e logar um usuário e validando senha forte

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,7 @@ import { knex } from '../config';
 import { z } from 'zod';
 import { randomUUID } from 'node:crypto';
 import bcrypt from 'bcrypt';
+import { isStrongPassword } from '../utils/functions/is-strong-password';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
@@ -12,51 +13,97 @@ export async function usersRoutes(app: FastifyInstance) {
       password: z.string(),
     });
 
-    const { name, email, password } = createUsersBodySchema.parse(request.body);
+    try {
+      const { name, email, password } = createUsersBodySchema.parse(request.body);
 
-    const emailExists = await knex('users').where('email', email).first();
-    if (emailExists) return reply.status(409).send({ message: 'Email already exists.' });
+      if (!isStrongPassword(password)) {
+        return reply.status(400).send({
+          statusCode: 400,
+          error: 'Bad Request',
+          message: [
+            'Password must contain at least 8 characters, including uppercase letters, lowercase letters, numbers, and special characters.',
+          ],
+        });
+      }
 
-    let sessionUuid = request.cookies.sessionUuid;
+      const emailExists = await knex('users').where('email', email).first();
+      if (emailExists) return reply.status(409).send({ message: 'Email já existe.' });
 
-    if (!sessionUuid) {
-      sessionUuid = randomUUID();
+      const sessionUuid = randomUUID();
 
       reply.cookie('sessionUuid', sessionUuid, {
         path: '/',
         maxAge: 60 * 60 * 24 * 7, // 7 days
       });
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const [createdUser] = await knex('users')
+        .insert({
+          uuid: randomUUID(),
+          name,
+          email,
+          password: hashedPassword,
+          session_uuid: sessionUuid,
+        })
+        .returning('*');
+
+      return reply.status(201).send(createdUser);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const formattedErrors = error.errors.map((err) => {
+          return `${err.path.join('.')} ${err.message}`;
+        });
+
+        return reply.status(400).send({
+          statusCode: 400,
+          error: 'Bad Request',
+          message: formattedErrors,
+        });
+      }
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const [createdUser] = await knex('users')
-      .insert({
-        uuid: randomUUID(),
-        name,
-        email,
-        password: hashedPassword,
-        session_uuid: sessionUuid,
-      })
-      .returning('*');
-
-    return reply.status(201).send(createdUser);
   });
 
   app.post('/login', async (request, reply) => {
-    const createUsersBodySchema = z.object({
-      email: z.string(),
+    const loginBodySchema = z.object({
+      email: z.string().email(),
       password: z.string(),
     });
 
-    const { email, password } = createUsersBodySchema.parse(request.body);
+    try {
+      const { email, password } = loginBodySchema.parse(request.body);
 
-    const user = await knex('users').where('email', email).first();
-    if (!user) return reply.status(404).send({ message: 'User not found.' });
+      const user = await knex('users').where('email', email).first();
+      if (!user) {
+        return reply.status(404).send({
+          statusCode: 404,
+          error: 'Not Found',
+          message: 'User not found.',
+        });
+      }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) return reply.status(401).send({ message: 'Invalid password.' });
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return reply.status(401).send({
+          statusCode: 401,
+          error: 'Unauthorized',
+          message: 'Invalid password.',
+        });
+      }
 
-    return reply.status(200).send({ session_uuid: user.session_uuid });
+      return reply.status(200).send({ session_uuid: user.session_uuid });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const formattedErrors = error.errors.map((err) => {
+          return `${err.path.join('.')} ${err.message}`;
+        });
+
+        return reply.status(400).send({
+          statusCode: 400,
+          error: 'Bad Request',
+          message: formattedErrors,
+        });
+      }
+    }
   });
 }
---
+++ src/utils/functions/is-strong-password.ts
@@ -0,0 +1,4 @@
+export function isStrongPassword(password: string): boolean {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+  return passwordRegex.test(password);
+}
